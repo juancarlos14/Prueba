d87d35a83fd217f5fa1370de01dc4c6dbfba73d4
juan.orihuela.perez <juan.orihuela.perez@CPX-JWDNEJNWN46.dir.svc.accenture.com>
2018-07-05 16:39:48 -0500

    Segundo Commit - Clase PruebaEmail.java

---
+++ Ejercicio/src/com/prueba/email/PruebaEmail.java
@@ -0,0 +1,10 @@
+package com.prueba.email;
+
+public class PruebaEmail {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo!!");
+	}
+
+}
