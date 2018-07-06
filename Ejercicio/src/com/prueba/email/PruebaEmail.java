b8231b40c453df47c95d460ba787f474445d8d80
juan.orihuela.perez <juan.orihuela.perez@CPX-JWDNEJNWN46.dir.svc.accenture.com>
2018-07-06 12:27:56 -0500

    Se agrega Adios.

--- Ejercicio/src/com/prueba/email/PruebaEmail.java
+++ Ejercicio/src/com/prueba/email/PruebaEmail.java
@@ -5,6 +5,7 @@ public class PruebaEmail {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundoiaiodfsfs!!");
+		System.out.println("Adios!!!");
 	}
 
 }
